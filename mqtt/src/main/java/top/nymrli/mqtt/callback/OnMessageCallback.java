e6b161588a254650b5954c53bf4afafb03c6cb82
Freedomisgood <1063052964@qq.com>
2021-10-10 16:12:28 +0800

    feat: MQTT Clinet demo TODO: Integrate MQTT into Spingboot

---
+++ mqtt/src/main/java/top/nymrli/mqtt/callback/OnMessageCallback.java
@@ -0,0 +1,28 @@
+package top.nymrli.mqtt.callback;
+
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.MqttCallback;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+
+/**
+ * @description:
+ * @author: MrLi
+ * @create: 2021-10-10 15:55
+ **/
+public class OnMessageCallback implements MqttCallback {
+    public void connectionLost(Throwable cause) {
+        // 连接丢失后，一般在这里面进行重连
+        System.out.println("连接断开，可以做重连");
+    }
+
+    public void messageArrived(String topic, MqttMessage message) throws Exception {
+        // subscribe后得到的消息会执行到这里面
+        System.out.println("接收消息主题:" + topic);
+        System.out.println("接收消息Qos:" + message.getQos());
+        System.out.println("接收消息内容:" + new String(message.getPayload()));
+    }
+
+    public void deliveryComplete(IMqttDeliveryToken token) {
+        System.out.println("deliveryComplete---------" + token.isComplete());
+    }
+}
---
+++ mqtt/src/test/java/top/nymrli/mqtt/MqttApplicationTests.java
@@ -0,0 +1,12 @@
+package top.nymrli.mqtt;
+
+
+import org.junit.Test;
+
+class MqttApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
