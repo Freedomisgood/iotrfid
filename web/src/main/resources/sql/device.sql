c942fd1fb8736abd1fab825111e266d45bfa8d67
Freedomisgood <1063052964@qq.com>
2021-10-10 21:02:45 +0800

    Merge remote-tracking branch 'origin/main'

--- web/src/main/resources/sql/device.sql
+++ web/src/main/resources/sql/device.sql
@@ -2,14 +2,26 @@ CREATE DATABASE IF NOT EXISTS `iot`;
 
 USE `iot`;
 
-CREATE TABLE IF NOT EXISTS `iotrfid_device`(
-    `id` BIGINT(11) NOT NULL AUTO_INCREMENT COMMENT '主键id',
-    `mac_id` VARCHAR(32) NOT NULL COMMENT '设备MAC地址',
-    `created_time` DATETIME DEFAULT NOW() COMMENT '创建时间',
-    `updated_time` DATETIME DEFAULT NOW() COMMENT '更新时间',
-    PRIMARY KEY (`id`)
+# CREATE TABLE IF NOT EXISTS `iotrfid_device`(
+#     `id` BIGINT(11) NOT NULL AUTO_INCREMENT COMMENT '主键id',
+#     `mac_id` VARCHAR(32) NOT NULL COMMENT '设备MAC地址',
+#     `created_time` DATETIME DEFAULT NOW() COMMENT '创建时间',
+#     `updated_time` DATETIME DEFAULT NOW() COMMENT '更新时间',
+#     PRIMARY KEY (`id`)
+# )ENGINE = InnoDB DEFAULT CHARSET = utf8;
+#
+# INSERT INTO `iotrfid_device` VALUES (1, "192.168.0.1", NOW(), NOW());
+# INSERT INTO `iotrfid_device` VALUES (2, "192.168.0.2", NULL, NULL);
+
+
+CREATE TABLE IF NOT EXISTS `device`(
+           `id` BIGINT(11) NOT NULL AUTO_INCREMENT COMMENT '主键id',
+           `mac_id` VARCHAR(32) NOT NULL COMMENT '设备MAC地址',
+           `created_time` DATETIME DEFAULT NOW() COMMENT '创建时间',
+           `updated_time` DATETIME DEFAULT NOW() COMMENT '更新时间',
+           PRIMARY KEY (`id`)
 )ENGINE = InnoDB DEFAULT CHARSET = utf8;
 
 
-INSERT INTO `iotrfid_device` VALUES (1, "192.168.0.1", NOW(), NOW());
-INSERT INTO `iotrfid_device` VALUES (2, "192.168.0.2", NULL, NULL);
+INSERT INTO `device` VALUES (NULL, "192.168.0.1", NOW(), NOW());
+INSERT INTO `device` VALUES (NULL, "192.168.0.2", default, default);
--- web/src/main/resources/sql/user.sql
+++ web/src/main/resources/sql/user.sql
@@ -2,9 +2,18 @@ CREATE DATABASE IF NOT EXISTS `iot`;
 
 USE `iot`;
 
-CREATE TABLE IF NOT EXISTS `iotrfid_user`(
-   `id` BIGINT(11) NOT NULL AUTO_INCREMENT COMMENT '主键id',
-   `username` VARCHAR(11) NOT NULL COMMENT '用户名',
-   `password` VARCHAR(11) NOT NULL COMMENT '密码',
-   PRIMARY KEY (`id`)
+# CREATE TABLE IF NOT EXISTS `iotrfid_user`(
+#    `id` BIGINT(11) NOT NULL AUTO_INCREMENT COMMENT '主键id',
+#    `username` VARCHAR(11) NOT NULL COMMENT '用户名',
+#    `password` VARCHAR(11) NOT NULL COMMENT '密码',
+#    PRIMARY KEY (`id`)
+# )ENGINE = InnoDB DEFAULT CHARSET = utf8;
+
+CREATE TABLE IF NOT EXISTS `user`(
+     `id` BIGINT(11) NOT NULL AUTO_INCREMENT COMMENT '主键id',
+     `username` VARCHAR(32) NOT NULL COMMENT '用户名',
+     `password` VARCHAR(32) NOT NULL COMMENT '密码',
+     PRIMARY KEY (`id`)
 )ENGINE = InnoDB DEFAULT CHARSET = utf8;
+
+INSERT INTO `user` VALUES (NULL, "Mrli", "password");
