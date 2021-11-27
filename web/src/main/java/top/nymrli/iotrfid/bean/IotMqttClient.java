1abba40b11bb86f88cef31148a3875b630636bbf
Freedomisgood <1063052964@qq.com>
2021-11-27 15:26:06 +0800

    feat: 增加了MqttConfiguration配置类, 将IotMqttClient的初始化连接放到了@PostConstruct里

--- web/src/main/java/top/nymrli/iotrfid/bean/IotMqttClient.java
+++ web/src/main/java/top/nymrli/iotrfid/bean/IotMqttClient.java
@@ -3,9 +3,10 @@ package top.nymrli.iotrfid.bean;
 import lombok.extern.slf4j.Slf4j;
 import org.eclipse.paho.client.mqttv3.*;
 import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
-import org.springframework.context.annotation.PropertySource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,47 +17,46 @@ import java.util.List;
  **/
 @Slf4j
 @Component
-@PropertySource("classpath:mqtt.properties")
 public class IotMqttClient {
-    public String host = "tcp://49.235.118.244:1883";
-    public String topic = "/rfid/#";
-    private String name = "";
-    private String passWord = "";
+    @Autowired
+    private MqttConfiguration mqttConfiguration;
 
     private MqttClient client;
     private static List<String> subedTopics = new ArrayList<>();
 
-    public IotMqttClient() {
+    @PostConstruct
+    private void init() {
         // clientId不能重复所以这里我设置为系统时间
         String clientId = String.valueOf(System.currentTimeMillis());
         int qos = 0;
         try {
             // host为主机名，clientid即连接MQTT的客户端ID，一般以唯一标识符表示，MemoryPersistence设置clientid的保存形式，默认为以内存保存
-            client = new MqttClient(host, clientId, new MemoryPersistence());
+            client = new MqttClient(mqttConfiguration.host, clientId, new MemoryPersistence());
             // 设置回调
             client.setCallback(new ReportMqtt(this));
             client.connect(makeUpMqttOption());
-            subscribe(topic, qos);
-        } catch (Exception e){
+            subscribe(mqttConfiguration.topic, qos);
+        } catch (Exception e) {
             log.info("ReportMqtt客户端连接异常，异常信息：" + e);
         }
     }
 
-    private MqttConnectOptions makeUpMqttOption(){
+    private MqttConnectOptions makeUpMqttOption() {
         // MQTT的连接设置
         MqttConnectOptions options = new MqttConnectOptions();
         // 设置是否清空session,这里如果设置为false表示服务器会保留客户端的连接记录，这里设置为true表示每次连接到服务器都以新的身份连接
         options.setCleanSession(true);
         // 设置连接的用户名
-        options.setUserName(name);
+        options.setUserName(mqttConfiguration.name);
         // 设置连接的密码
-        options.setPassword(passWord.toCharArray());
+        options.setPassword(mqttConfiguration.passWord.toCharArray());
         // 设置超时时间 单位为秒
         options.setConnectionTimeout(10);
         // 设置会话心跳时间 单位为秒 服务器会每隔1.5*20秒的时间向客户端发送个消息判断客户端是否在线，但这个方法并没有重连的机制
         options.setKeepAliveInterval(3600);
         return options;
     }
+
     /**
      * 发布，默认qos为0，非持久化
      *
